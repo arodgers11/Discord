0dd900b03c4b62e479ad040abcadc442226960cb
arodgers11 <32276124+arodgers11@users.noreply.github.com>
2020-07-03 05:26:57 -0400

    Update Music.py

--- cogs/Music.py
+++ cogs/Music.py
@@ -5,7 +5,6 @@ import asyncio
 import time
 
 if not discord.opus.is_loaded():
-
 	# the 'opus' library here is opus.dll on windows
 	# or libopus.so on linux in the current directory
 	# you should replace this with the location the
