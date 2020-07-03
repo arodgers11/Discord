9d35e5a42d1e8e70c16a35103c923671b8eff90e
arodgers11 <32276124+arodgers11@users.noreply.github.com>
2020-07-03 05:18:48 -0400

    Update OWL_wiki.py

--- cogs/OWL_wiki.py
+++ cogs/OWL_wiki.py
@@ -50,7 +50,7 @@ class OWL_wiki(commands.Cog):
 		table=None
 		for i in range(0,len(teams)):
 			try:
-				table=tables[i]['Active Squad'][['ID','Role']]
+				table=tables[i]['Active Squad'][['ID','Role']].sort(['Role','ID'])
 			except:
 				None
 								
