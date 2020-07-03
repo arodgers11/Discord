4e3df58fd352a611e41ed0637d30501a95a483b8
arodgers11 <32276124+arodgers11@users.noreply.github.com>
2020-07-03 03:44:13 -0400

    Update OWL_wiki.py

--- cogs/OWL_wiki.py
+++ cogs/OWL_wiki.py
@@ -26,71 +26,71 @@ teams=[
 '/overwatch/Washington_Justice'
 ]
 
-class OWL_wiki(commands.Cog):    
-    def __init__(self, bot):
-        self.bot = bot
+class OWL_wiki(commands.Cog):	
+	def __init__(self, bot):
+		self.bot = bot
 
-    #@commands.command(pass_context=True)
+	#@commands.command(pass_context=True)
 
-    @commands.Cog.listener()
-    async def on_ready(self):
-        print('OWL_wiki Loaded')
+	@commands.Cog.listener()
+	async def on_ready(self):
+		print('OWL_wiki Loaded')
 		
-    @commands.command(aliases=['get_team','roster'])
-    async def team(self,ctx,*,team):
-        """Prints the active roster for an OWL team. Accepts partial names."""
-        url='https://liquipedia.net'
-        t='No Team Found'
-        for i in teams:
-            if team.upper() in i.upper():
-                t=i
-                break
-        #tables=pd.read_html(requests.get('https://liquipedia.net/overwatch/Dallas_Fuel').content)
-        tables=pd.read_html(requests.get(url+t).content)
-        for i in range(0,len(teams)):
-            try:
-                table=tables[i]['Active Squad'][['ID','Role']]
-            except:
-                None
-                                
-                if not table:
-                        await ctx.send("Team Not Found")
-                        return
-                
-        s=['']*len(table)
-        for i in range(0,len(table)):
-            s[i]=table['ID'][i]+' '+'.'*(25-len(table['ID'][i])-len(table['Role'][i]))+' '+table['Role'][i]
-        TEAM_NAME='**'+t[11:len(t)].replace('_',' ')+'**'
-        table[TEAM_NAME]=s
-        df=table[[TEAM_NAME]]
-        df.index=['']*len(df)
-        await ctx.send(df)
+	@commands.command(aliases=['get_team','roster'])
+	async def team(self,ctx,*,team):
+		"""Prints the active roster for an OWL team. Accepts partial names."""
+		url='https://liquipedia.net'
+		t='No Team Found'
+		for i in teams:
+			if team.upper() in i.upper():
+				t=i
+				break
+		#tables=pd.read_html(requests.get('https://liquipedia.net/overwatch/Dallas_Fuel').content)
+		tables=pd.read_html(requests.get(url+t).content)
+		for i in range(0,len(teams)):
+			try:
+				table=tables[i]['Active Squad'][['ID','Role']]
+			except:
+				None
+								
+				if not table:
+						await ctx.send("Team Not Found")
+						return
+				
+		s=['']*len(table)
+		for i in range(0,len(table)):
+			s[i]=table['ID'][i]+' '+'.'*(25-len(table['ID'][i])-len(table['Role'][i]))+' '+table['Role'][i]
+		TEAM_NAME='**'+t[11:len(t)].replace('_',' ')+'**'
+		table[TEAM_NAME]=s
+		df=table[[TEAM_NAME]]
+		df.index=['']*len(df)
+		await ctx.send(df)
 
-    @commands.command(aliases=['liquipedia','liq'])
-    async def wiki(self,ctx,*team):
-        """Gets the Liquipedia link for an OWL team.
-        Can also use .teams , or .players to get the teams or players links, respectively"""
-        url='<https://liquipedia.net'
-        if len(team)==0:
-            await ctx.send(url+'/overwatch/Overwatch_League>')
-            return
-        if team=='players':
-            await ctx.send(url+'/overwatch/Players>')
-            return
-        if team=='teams':
-            await ctx.send(url+'/overwatch/Portal:Teams/Overwatch_League>')
-            return
-        for i in teams:
-            if team.upper() in i[11:].upper():
-                t=i
-                break
-            else:
-                t='No Team Found'
-        if t!='No Team Found':
-            await ctx.send(url+t+'>')
-        else:
-            print('NO TEAM FOUND')
-            await ctx.send('No Team Found')
+	@commands.command(aliases=['liquipedia','liq'])
+	async def wiki(self,ctx,*team):
+		"""Gets the Liquipedia link for an OWL team.
+		Can also use .teams , or .players to get the teams or players links, respectively"""
+		url='<https://liquipedia.net'
+		if len(team)==0:
+			await ctx.send(url+'/overwatch/Overwatch_League>')
+			return
+		if team=='players':
+			await ctx.send(url+'/overwatch/Players>')
+			return
+		if team=='teams':
+			await ctx.send(url+'/overwatch/Portal:Teams/Overwatch_League>')
+			return
+		for i in teams:
+			if team.upper() in i[11:].upper():
+				t=i
+				break
+			else:
+				t='No Team Found'
+		if t!='No Team Found':
+			await ctx.send(url+t+'>')
+		else:
+			print('NO TEAM FOUND')
+			await ctx.send('No Team Found')
 
 def setup(bot):
-    bot.add_cog(OWL_wiki(bot))
+	bot.add_cog(OWL_wiki(bot))
