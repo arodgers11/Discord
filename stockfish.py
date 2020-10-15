a415fe0b45ed9c26de22929bd0049c142c0efe5c
arodgers11 <32276124+arodgers11@users.noreply.github.com>
2020-10-15 02:30:58 -0400

    Update stockfish.py

--- stockfish.py
+++ stockfish.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 import os
 
-TOKEN=os.environ['BOT_TOKEN']
+TOKEN='NzE4MDIyNzE3ODUyNDgzNjQ2.XwVLCQ.hzuLdgeKfMdj-aP8yZohFyvHS0Q'
 
 allowed_channels=['stockfish-war-room','bots']
 
@@ -15,17 +15,19 @@ async def on_ready():
 client=commands.Bot(command_prefix='.')
 
 @client.command(aliases=['c'])
-async def clear(ctx,amount):
+async def clear(ctx,amount=20):
     await ctx.channel.purge(limit=amount)
+
+client.load_extension('cogs.OWL_wiki')
 
 @client.command()
 async def load(ctx,ext):
     if ext.lower()=='music':
         extension='Music'
-    if ext.lower()=='wiki':
-        extension='OWL_wiki'
-    if ext.lower()=='stats':
-        extension='OWL_stats'
+    # if ext.lower()=='wiki':
+        # extension='OWL_wiki'
+    # if ext.lower()=='stats':
+        # extension='OWL_stats'
     client.load_extension(f'cogs.{extension}')
     print(f'{extension} Loaded')
 
@@ -44,6 +46,4 @@ async def unload(ctx,ext):
 async def on_ready():
     print('READY')
         
-client.run(TOKEN,
-           bot=True,
-           reconnect=True)
+client.run(TOKEN,bot=True,reconnect=True)
