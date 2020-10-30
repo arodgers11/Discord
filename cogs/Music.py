9add8325bbf530d6b874e5cab7ea6b88f155c592
arodgers11 <32276124+arodgers11@users.noreply.github.com>
2020-10-30 03:04:35 -0400

    fucking fixed spam_lock for hopefully the last time

--- cogs/Music.py
+++ cogs/Music.py
@@ -26,9 +26,17 @@ ffmpeg_options = {
 
 ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
 
+spam_lock=time.time()
+
 def play_file(ctx,file_path):
-    ctx.voice_client.play(discord.FFmpegPCMAudio(source=file_path))
-    
+    global spam_lock
+    if time.time()-spam_lock<10:
+        spam_lock=time.time()
+        await ctx.send("FOR FUCK'S SAKE STOP SPAMMING COMMANDS!!")
+    else:
+        spam_lock=time.time()
+        ctx.voice_client.play(discord.FFmpegPCMAudio(source=file_path))
+
 class YTDLSource(discord.PCMVolumeTransformer):
     def __init__(self, source, *, data, volume=0.5):
         super().__init__(source, volume)
@@ -366,5 +374,4 @@ class Music(commands.Cog):
                 ctx.voice_client.stop()
             
 def setup(bot):
-    spam_lock=time.time()
     bot.add_cog(Music(bot))
