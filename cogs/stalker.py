6cbd5d10e37a2182e6472a6df8cde8cb8d2f9bf2
arodgers11 <32276124+arodgers11@users.noreply.github.com>
2021-06-21 01:29:53 -0400

    Update stalker.py

--- cogs/stalker.py
+++ cogs/stalker.py
@@ -11,14 +11,16 @@ class stalker(commands.Cog):
 
 #@client.event
 #async def on_member_update(before, after):
-    #if before.status != after.status:  # to only run on status
-        #if
+    #if before.id=""
+        #if before.status != after.status:  # to only run on status
 
     @commands.command()
     async def off(self,ctx):
-        for user in ctx.guild.fetch_members(limit=None):
+        #async for user in ctx.guild.fetch_members(limit=None):
+        for user in ctx.guild.members:
             #print(user.status)
-            print(user.name, " ",user.id)
+            if user.status != discord.Status.offline:
+                print(user.name, " ",user.id)
 
 def setup(bot):
     bot.add_cog(stalker(bot))
