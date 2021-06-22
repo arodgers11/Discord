9e7435a8be3dafd1f4a20c7d9ba58fe0b53db08b
arodgers11 <32276124+arodgers11@users.noreply.github.com>
2021-06-22 01:39:00 -0400

    fixed stuff

--- stockfish.py
+++ stockfish.py
@@ -16,7 +16,17 @@ async def on_ready():
 async def clear(ctx,amount=20):
     await ctx.channel.purge(limit=amount)
 
-client.load_extension('cogs.Music')
+@client.event
+async def on_member_update(before, after):
+    if before.name=='andyarchy' or before.name=='arodgers':
+        if before.status != after.status or before.activity != after.activity:
+            if after.status != "offline" and after.status != 'do_not_disturb':
+                user = client.get_user(215957034820960256)
+                print(after.activity)
+                print(after.status)
+                await user.send( after.name + " is online")
+
+#client.load_extension('cogs.Music')
 #client.load_extension('cogs.OWL_wiki')
 client.load_extension('cogs.stalker')
 
